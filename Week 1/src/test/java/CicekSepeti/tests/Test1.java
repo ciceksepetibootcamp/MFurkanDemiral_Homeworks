2c21a53f84d3bf881d68f1e4783b05055fc4ddc2
Muhammet Furkan DEMİRAL <37184598+muhammetfurkandemiral@users.noreply.github.com>
2021-11-30 15:48:57 +0300

    Week 1

---
+++ Week 1/src/test/java/CicekSepeti/tests/Test1.java
@@ -0,0 +1,33 @@
+package CicekSepeti.tests;
+
+import CicekSepeti.utils.ConfReader;
+import CicekSepeti.utils.Driver;
+import org.junit.*;
+import org.openqa.selenium.By;
+
+public class Test1 extends BaseTest {
+
+    @Test
+    public void login() {
+        Driver.get().findElement(By.cssSelector(".user-menu__item:nth-child(2)")).click();
+        Driver.get().findElement(By.className("users-process-list__btn")).click();
+        Driver.get().findElement(By.id("EmailLogin")).sendKeys(ConfReader.get("email"));
+        Driver.get().findElement(By.id("Password")).sendKeys(ConfReader.get("password"));
+        Driver.get().findElement(By.className("login__btn")).click();
+    }
+
+    @Test
+    public void favoriUrunKontrol() throws InterruptedException {
+        login();
+        Thread.sleep(2000);
+        Driver.get().findElement(By.className("js-product-search-input")).sendKeys("çiçek");
+        Driver.get().findElement(By.className("product-search__button")).click();
+        Driver.get().findElement(By.cssSelector(".products__item:nth-child(2)")).click();
+        String productName = Driver.get().findElement(By.className("js-ellipsize-text")).getText();
+        Driver.get().findElement(By.className("js-favorite-add")).click();
+        Driver.get().findElement(By.className("user-menu__link--favorite")).click();
+        String favProductName = Driver.get().findElement(By.className("products__item-title")).getText();
+        Driver.get().findElement(By.className("add-favorite")).click();
+        Assert.assertEquals(productName, favProductName);
+    }
+}
